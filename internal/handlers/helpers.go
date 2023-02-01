d86888834ebaaf30de4c3057004a78dfea7a3b9f
Jacob Lindgren <jlindgre@redhat.com>
2023-02-01 10:09:53 -0600

    add sendJson helper functions

--- internal/handlers/helpers.go
+++ internal/handlers/helpers.go
@@ -1,10 +1,25 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
 	l "github.com/redhatinsights/mbop/internal/logger"
 )
+
+func sendJSON(w http.ResponseWriter, data any) {
+	sendJSONWithStatusCode(w, data, 200)
+}
+
+func sendJSONWithStatusCode(w http.ResponseWriter, data any, code int) {
+	b, _ := json.Marshal(data)
+
+	w.WriteHeader(code)
+	_, err := w.Write(b)
+	if err != nil {
+		l.Log.Error(err, "error writing response")
+	}
+}
 
 func do500(w http.ResponseWriter, msg string) {
 	doError(w, msg, 500)
@@ -15,9 +30,5 @@ func do400(w http.ResponseWriter, msg string) {
 }
 
 func doError(w http.ResponseWriter, msg string, code int) {
-	w.WriteHeader(code)
-	_, err := w.Write(newResponse(msg))
-	if err != nil {
-		l.Log.Error(err, "error writing response")
-	}
+	sendJSONWithStatusCode(w, newResponse(msg), code)
 }
--- internal/handlers/response.go
+++ internal/handlers/response.go
@@ -1,16 +1,9 @@
 package handlers
 
-import "encoding/json"
-
 type response struct {
 	Message string `json:"message"`
 }
 
-func (r response) toByteArray() []byte {
-	b, _ := json.Marshal(r)
-	return b
-}
-
-func newResponse(msg string) []byte {
-	return response{Message: msg}.toByteArray()
+func newResponse(msg string) *response {
+	return &response{Message: msg}
 }
